9506f8cc513f68545a4d067ea10b709ce55a3410
hari <hari@nagarajan.io>
2018-08-31 18:38:08 -0400

    Adding dynamic data loading from front end
    Signed-off-by: hari <hari@nagarajan.io>

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,59 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        Hari's Website!
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
+  <section id="projects">
+    <ProjectPreview
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :previewText="post.previewText"
+      :thumbnailUrl="post.thumbnailUrl"
+      :id="post.id"
+    />
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
-
+  import ProjectPreview from "@/components/Projects/ProjectPreview";
 export default {
-  components: {
-    AppLogo
+  components:{
+    ProjectPreview
+  },
+  data() {
+    return {
+      posts: [{
+          title: 'I am cool af',
+          previewText: 'I got a cat!',
+          thumbnailUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
+          id: '0'
+        },
+        {
+          title: 'I am cool af',
+          previewText: 'I got a another cat!',
+          thumbnailUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg',
+          id: '1'
+        }
+      ]};
   }
-}
+};
 </script>
 
 <style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+  .container{
+    margin-top: 4.5rem;
+  }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+  #projects{
+    padding-top: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 
-.links {
-  padding-top: 15px;
-}
+  @media (min-width: 35rem){
+    #projects{
+      flex-direction: row;
+    }
+  }
 </style>
 
