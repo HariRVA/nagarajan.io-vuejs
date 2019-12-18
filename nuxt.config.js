7891e6123679fb338f054aeae8d1275df4ea0e47
hari <hari@nagarajan.io>
2019-12-17 23:01:06 -0500

    Load essential css

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,13 +21,11 @@ module.exports = {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1' },
+      { name: 'viewport', content: 'user-scalable=yes, width=device-width, initial-scale=1, maximum-scale=5' },
       { hid: 'description', name: 'description', content: 'Hari\'s Personal Website!' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: '/fonts.css'},
-      { rel: 'stylesheet', type: 'text/css', href: '/nanogallery2.min.css'}
       ]
   },
   /*
