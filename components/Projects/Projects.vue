e5c389c36ee58e527686d89db642c8b967d5199c
hari <hari@nagarajan.io>
2020-01-03 20:15:13 -0500

    Add projects section

--- components/Projects/Projects.vue
+++ components/Projects/Projects.vue
@@ -27,7 +27,7 @@
             projectName: "Twitter AI",
             repoURL: "https://github.com/Hari-Nagarajan/gpt2-twitter",
             projectDescription: "I like Twitter, but I never actually tweet anything, so I figured why not use Machine Learning to tweet for me." +
-              "I used the GPT-2 Model from OpenAI as a base and then tuned it with content from some of the users I follow. Now I have a bot periodically" +
+              "I used the GPT-2 Model from OpenAI as a base and then tuned it with content from some of the users I follow. Now I have a bot periodically " +
               "generating content to post and I am now active on Twitter. Follow me for occasionally high quality content."
           }
         ]
